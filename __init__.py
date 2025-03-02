6fe343e368e32628e5892ea4b01d35fc9a35d26d
ǝunboɯiɔs <shimokune1@gmail.com>
2025-03-02 23:34:45 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -2,20 +2,17 @@ from .nodes import (ControlNetSwitch, ImageSwitch, LatentSwitch, FluxSamplerPuLI
                    NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS)
 from .Lighting_and_Color import FluxLightingAndColor
 from .load_input_output_image import LoadInputOutputImage
-from .clip_text_encode_io import CLIPTextEncodeWithPromptIO
 
 # ノードマッピングに追加
 NODE_CLASS_MAPPINGS.update({
     "LoadInputOutputImage": LoadInputOutputImage,
     "FluxLightingAndColor": FluxLightingAndColor,
-    "CLIPTextEncodeWithPromptIO": CLIPTextEncodeWithPromptIO,
 })
 
 # 表示名マッピングに追加
 NODE_DISPLAY_NAME_MAPPINGS.update({
     "LoadInputOutputImage": "Load Input/Output Image",
     "FluxLightingAndColor": "Flux Lighting & Color",
-    "CLIPTextEncodeWithPromptIO": "CLIP Text Encode with Prompt I/O",
 })
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
